7b9046ad5bbee8875d556c55fd2107d65f4f04e7
EdderGit <edder.rodea@broxel.com>
2017-04-26 10:17:15 -0500

    Se actualiza usuario controller

--- ProyectoConsolaMvc/Controllers/UsuarioController.cs
+++ ProyectoConsolaMvc/Controllers/UsuarioController.cs
@@ -1,1 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
 
+namespace ProyectoConsolaMvc.Controllers
+{
+    public class UsuarioController : Controller
+    {
+    }
+}
