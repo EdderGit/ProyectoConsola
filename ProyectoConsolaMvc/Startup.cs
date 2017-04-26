d241eb87e2f49c97e8c617d530109b94726bc9fd
edder.rodea@broxel.com <edder.rodea@broxel.com>
2017-04-26 10:38:08 -0500

    Se agrega todo el proyecto mvc

---
+++ ProyectoConsolaMvc/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ProyectoConsolaMvc.Startup))]
+namespace ProyectoConsolaMvc
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
