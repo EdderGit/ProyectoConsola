d5f59abd7ce2356f304a37824e97b0b199df04fb
edder.rodea@broxel.com <edder.rodea@broxel.com>
2017-04-26 10:08:04 -0500

    Se agrega clase CLaptop

---
+++ ProyectoConsolaMvc/Clases/CLaptop.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProyectoConsolaMvc.Clases
+{
+    public class CLaptop
+    {
+        public int NumeroSerie { get; set; }
+    }
+}
+}
