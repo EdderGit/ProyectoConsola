e88b570782e14424e58d72971c4814040c4e32da
edder.rodea@broxel.com <edder.rodea@broxel.com>
2017-04-26 09:45:03 -0500

    Esta es mi primera clase agregada

---
+++ ProyectoConsolaMvc/Clases/COperaciones.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProyectoConsolaMvc.Clases
+{
+    public class COperaciones
+    {
+        public string Valor { get; set; }
+    ;
+
+
+    }
+}
