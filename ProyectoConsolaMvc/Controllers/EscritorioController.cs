e14301c3190ea6a25665c73f72062455d66c3e96
edder.rodea@broxel.com <edder.rodea@broxel.com>
2017-04-26 10:40:57 -0500

    Se Agrega controlador escritorio

---
+++ ProyectoConsolaMvc/Controllers/EscritorioController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProyectoConsolaMvc.Controllers
+{
+    public class EscritorioController : Controller
+    {
+        // GET: Escritorio
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
